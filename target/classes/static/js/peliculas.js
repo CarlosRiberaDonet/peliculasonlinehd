d704dc840e6a90d230c8c40d50c9caf8a6441b19
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2025-08-13 16:47:36 +0200

    fly.io
    modifico url para que el front end consuma el backend en fly.io

--- target/classes/static/js/peliculas.js
+++ target/classes/static/js/peliculas.js
@@ -4,7 +4,7 @@ import { abrirTrailer } from './trailer.js';
 const contenedor = document.getElementById('peliculas-container');
 
 // Petición al backend para obtener películas (página 2)
-fetch('http://localhost:8080/peliculas?page=2')
+fetch('https://peliculasonlinehd.fly.dev/peliculas?page=2')
   .then(response => response.json())  // Convertir respuesta a JSON
   .then(data => {
     // Recorrer cada película en el array results
